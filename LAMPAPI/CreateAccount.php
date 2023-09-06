d5247d3da64b48bc62101cc0ed7fc48ba6060da0
EdwardWeir01 <93448600+EdwardWeir01@users.noreply.github.com>
2023-09-06 17:06:02 -0400

    Update CreateAccount.php
    I got the CreateAccount to work

--- LAMPAPI/CreateAccount.php
+++ LAMPAPI/CreateAccount.php
@@ -3,27 +3,21 @@
 	
 	$password = $inData["password"];
 	$login = $inData["login"];
-    $firstName = $inData["firstName"];
+  $firstName = $inData["firstName"];
 	$lastName = $inData["lastName"];
-
-  //echo ("Before Connection");
-  //mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-  //echo ("After Report");
-	//$conn = new mysqli("localhost", "root", "*42Miles", "COP4331");
-//  $conn = mysqli_connect("127.0.0.1", "root", "", "COP4331")
+  
   $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-  //echo ("Connection RA");
 	if ($conn->connect_error) 
 	{
-    echo ("Connection Error");
+   // echo ("Connection Error");
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
-    echo ("not Connection Error");
-		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-   // $stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["Login"], $inData["Password"]);
+   // echo ("not Connection Error");
+    
+	 $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+	 $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
