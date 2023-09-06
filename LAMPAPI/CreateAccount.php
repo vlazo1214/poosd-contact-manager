a9edb1c315d34426090a03b81ecf0b0c8e19f42c
EdwardWeir01 <93448600+EdwardWeir01@users.noreply.github.com>
2023-09-05 20:43:23 -0400

    Got to no connection error

--- LAMPAPI/CreateAccount.php
+++ LAMPAPI/CreateAccount.php
@@ -6,15 +6,24 @@
     $firstName = $inData["firstName"];
 	$lastName = $inData["lastName"];
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+  //echo ("Before Connection");
+  //mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+  //echo ("After Report");
+	//$conn = new mysqli("localhost", "root", "*42Miles", "COP4331");
+//  $conn = mysqli_connect("127.0.0.1", "root", "", "COP4331")
+  $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+  //echo ("Connection RA");
 	if ($conn->connect_error) 
 	{
+    echo ("Connection Error");
 		returnWithError( $conn->connect_error );
 	} 
 	else
 	{
+    echo ("not Connection Error");
 		$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
 		$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+   // $stmt->bind_param("ssss", $inData["FirstName"], $inData["LastName"], $inData["Login"], $inData["Password"]);
 		$stmt->execute();
 		$stmt->close();
 		$conn->close();
