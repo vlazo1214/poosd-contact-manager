35dd2837490616eca87d76c477cf7aca7938b7dc
Sean Feldman <the.feldman23@gmail.com>
2023-09-09 17:58:37 -0400

    signup

--- LAMPAPI/AddColor.php
+++ LAMPAPI/AddColor.php
@@ -1,39 +1,36 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$color = $inData["color"];
-	$userId = $inData["userId"];
+$inData = getRequestInfo();
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-		$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
-		$stmt->bind_param("ss", $userId, $color);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+$color = $inData["color"];
+$userId = $inData["userId"];
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+$conn = new mysqli("localhost", "root", "*42Miles", "COP4331");
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+} else {
+	$stmt = $conn->prepare("INSERT into Colors (UserId,Name) VALUES(?,?)");
+	$stmt->bind_param("ss", $userId, $color);
+	$stmt->execute();
+	$stmt->close();
+	$conn->close();
+	returnWithError("");
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+function returnWithError($err)
+{
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
+
 ?>
--- LAMPAPI/CreateAccount.php
+++ LAMPAPI/CreateAccount.php
@@ -1,44 +1,41 @@
 <?php
-	$inData = getRequestInfo();
-	
-	$password = $inData["password"];
-	$login = $inData["login"];
-  $firstName = $inData["firstName"];
-	$lastName = $inData["lastName"];
-  
-  $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error) 
-	{
-   // echo ("Connection Error");
-		returnWithError( $conn->connect_error );
-	} 
-	else
-	{
-   // echo ("not Connection Error");
-    
-	 $stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
-	 $stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
-		$stmt->execute();
-		$stmt->close();
-		$conn->close();
-		returnWithError("");
-	}
+$inData = getRequestInfo();
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+$password = $inData["password"];
+$login = $inData["login"];
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-	
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-	
-?> 
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error) {
+	// echo ("Connection Error");
+	returnWithError($conn->connect_error);
+} else {
+	// echo ("not Connection Error");
+
+	$stmt = $conn->prepare("INSERT INTO Users (FirstName, LastName, Login, Password) VALUES(?,?,?,?)");
+	$stmt->bind_param("ssss", $firstName, $lastName, $login, $password);
+	$stmt->execute();
+	$stmt->close();
+	$conn->close();
+	returnWithError("");
+}
+
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+function returnWithError($err)
+{
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
+
+?>
