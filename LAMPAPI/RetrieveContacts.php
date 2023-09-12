c1ec88d957f41d09d78a9f519ea84ad79e82a0fd
BenjaminCarmenate <79183964+BenjaminCarmenate@users.noreply.github.com>
2023-09-12 19:52:00 -0400

    RetrieveContacts

---
+++ LAMPAPI/RetrieveContacts.php
@@ -0,0 +1,58 @@
+<?php
+	$inData = getRequestInfo();
+
+
+	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error)
+	{
+		returnWithError( $conn->connect_error );
+	}
+	else
+	{
+    $stmt = $conn->prepare("select Name from Colors where UserID=? and GroupID=?");
+		$stmt->bind_param("ss", $inData["userId"], $inData["groupId"]);
+		$stmt->execute();
+
+		$result = $stmt->get_result();
+
+		while($row = $result->fetch_assoc())
+		{
+			if( $searchCount > 0 )
+			{
+				$searchResults .= ",";
+			}
+			$searchCount++;
+			$searchResults .= '"' . $row["Name"] . '"';
+		}
+
+		if( $searchCount == 0 )
+		{
+			returnWithError( "No Records Found" );
+		}
+		else
+		{
+			returnWithInfo( $searchResults );
+		}
+
+		$stmt->close();
+		$conn->close();
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+
+?>
