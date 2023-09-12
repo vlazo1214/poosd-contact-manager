973ac1edee0b0c57c2543d39e90b15e4ba99505f
EdwardWeir01 <93448600+EdwardWeir01@users.noreply.github.com>
2023-09-12 14:25:57 -0400

    Delete for User and Contact tables

---
+++ LAMPAPI/DeleteContact.php
@@ -0,0 +1,41 @@
+<?php
+	$inData = getRequestInfo();
+	
+  $ID = $inData["ID"];
+  
+  $conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+	if ($conn->connect_error) 
+	{
+   // echo ("Connection Error");
+		returnWithError( $conn->connect_error );
+	} 
+	else
+	{
+   // echo ("not Connection Error");
+    
+	 $stmt = $conn->prepare("DELETE FROM Contacts WHERE ID=?");
+	 $stmt->bind_param("s", $ID);
+		$stmt->execute();
+		$stmt->close();
+		$conn->close();
+		returnWithError("");
+	}
+
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function sendResultInfoAsJson( $obj )
+	{
+		header('Content-type: application/json');
+		echo $obj;
+	}
+	
+	function returnWithError( $err )
+	{
+		$retValue = '{"error":"' . $err . '"}';
+		sendResultInfoAsJson( $retValue );
+	}
+	
+?> 
