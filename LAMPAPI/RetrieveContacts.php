e9745771e81010fef8230b741796b7855a66db65
BenjaminCarmenate <79183964+BenjaminCarmenate@users.noreply.github.com>
2023-09-26 22:32:28 -0400

    Clean up

--- LAMPAPI/RetrieveContacts.php
+++ LAMPAPI/RetrieveContacts.php
@@ -1,70 +1,62 @@
 <?php
-	$inData = getRequestInfo();
+$inData = getRequestInfo();
 
-  $userId = $inData["userId"];
-	$groupId = $inData["groupId"];
-  $searchResults = '{"list" : [';
-  $searchCount = 0;
+$userId = $inData["userId"];
+$groupId = $inData["groupId"];
+$searchResults = '{"list" : [';
+$searchCount = 0;
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+} else {
+	//echo ("Here1");
+	if ($groupId == 0) {
+
+		$stmt = $conn->prepare("SELECT ID, FirstName, LastName, Phone, Email from Contacts where UserID=? and GroupID is NULL;");
+		$stmt->bind_param("i", $userId);
+	} else {
+		$stmt = $conn->prepare("SELECT ID, FirstName, LastName, Phone, Email from Contacts where UserID=? and GroupID=?;");
+		$stmt->bind_param("ii", $userId, $groupId);
 	}
-	else
-	{
-   //echo ("Here1");
-   if($groupId == 0){
+	$stmt->execute();
 
-    $stmt = $conn->prepare("SELECT FirstName, LastName, Phone, Email from Contacts where UserID=? and GroupID is NULL;");
-    $stmt->bind_param("i", $userId);
-    } else {
-       $stmt = $conn->prepare("SELECT FirstName, LastName, Phone, Email from Contacts where UserID=? and GroupID=?;");
-        $stmt->bind_param("ii", $userId, $groupId);
-   }
-		$stmt->execute();
+	$result = $stmt->get_result();
 
-		$result = $stmt->get_result();
-
-		while($row = $result->fetch_assoc())
-		{
-			if( $searchCount > 0 )
-			{
-				$searchResults .= ",";
-			}
-			$searchCount++;
-			$searchResults .= '{"FirstName" : "' . $row["FirstName"] . '",' . '"LastName" : "' . $row["LastName"] . '",' . '"Phone" : "' . $row["Phone"] . '", "Email" : ' . '"' . $row["Email"] . '"}';
-      }
-
-		if( $searchCount == 0 )
-		{
-			returnWithError( "No Records Found UserId:" . $userId . " GroupId: " . $groupId );
+	while ($row = $result->fetch_assoc()) {
+		if ($searchCount > 0) {
+			$searchResults .= ",";
 		}
-		else
-		{
-      $searchResults .= ']}';
-			sendResultInfoAsJson( $searchResults );
-		}
-
-		$stmt->close();
-		$conn->close();
+		$searchCount++;
+		$searchResults .= '{"ContactID" : "' . $row["ID"] . '",' . '"FirstName" : "' . $row["FirstName"] . '",' . '"LastName" : "' . $row["LastName"] . '",' . '"Phone" : "' . $row["Phone"] . '", "Email" : ' . '"' . $row["Email"] . '"}';
 	}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
+	if ($searchCount == 0) {
+		returnWithError("No Records Found UserId:" . $userId . " GroupId: " . $groupId);
+	} else {
+		$searchResults .= ']}';
+		sendResultInfoAsJson($searchResults);
 	}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+	$stmt->close();
+	$conn->close();
+}
 
-	function returnWithError( $err )
-	{
-		$retValue = '{"error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
+
+function returnWithError($err)
+{
+	$retValue = '{"error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
 
 ?>
--- LAMPAPI/UpdateContact.php
+++ LAMPAPI/UpdateContact.php
@@ -1,56 +1,52 @@
 <?php
-	$inData = getRequestInfo();
+$inData = getRequestInfo();
 
-  $firstName = $inData["firstName"];
-  $lastName = $inData["lastName"];
-  $phone = $inData["phone"];
-  $email = $inData["email"];
-  $groupId = $inData["groupId"];
-  $contactId = $inData["contactId"];
-  $userId = $inData["userId"];
+$firstName = $inData["firstName"];
+$lastName = $inData["lastName"];
+$phone = $inData["phone"];
+$email = $inData["email"];
+$groupId = $inData["groupId"];
+$contactId = $inData["contactId"];
+$userId = $inData["userId"];
 
 
-	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	if ($conn->connect_error)
-	{
-		returnWithError( $conn->connect_error );
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
+if ($conn->connect_error) {
+	returnWithError($conn->connect_error);
+} else {
+
+	$stmt = "UPDATE Contacts SET FirstName='$firstName', LastName='$lastName', Phone='$phone', Email='$email', GroupID=NULL WHERE ID='$contactId';";
+	echo $stmt;
+
+	if ($conn->query($stmt) === TRUE) {
+		echo "Record updated successfully";
+	} else {
+		echo "Error updating record: " . $conn->error;
 	}
-	else
-	{
+	$conn->close();
+}
 
-    $stmt = "UPDATE Contacts SET FirstName='$firstName', LastName='$lastName', Phone='$phone', Email='$email', GroupID='$groupId' WHERE ID='$contactId';";
-    echo $stmt;
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
 
-    if ($conn->query($stmt) === TRUE) {
-      echo "Record updated successfully";
-    }
-    else {
-      echo "Error updating record: " . $conn->error;
-    }
-		$conn->close();
-	}
+function sendResultInfoAsJson($obj)
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
 
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
-	}
+function returnWithError($err)
+{
+	$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
+	sendResultInfoAsJson($retValue);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
-
-	function returnWithError( $err )
-	{
-		$retValue = '{"id":0,"firstName":"","lastName":"","error":"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
-
-	function returnWithInfo( $searchResults )
-	{
-		$retValue = '{"results":[' . $searchResults . '],"error":""}';
-		sendResultInfoAsJson( $retValue );
-	}
+function returnWithInfo($searchResults)
+{
+	$retValue = '{"results":[' . $searchResults . '],"error":""}';
+	sendResultInfoAsJson($retValue);
+}
 
 ?>
